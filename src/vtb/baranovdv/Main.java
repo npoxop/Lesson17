cbd3f0fe8588e0c4d626f637059f90d03fd8215b
npoxop <npoxop7@gmail.com>
2021-02-03 19:23:09 +0300

    Initial commit

---
+++ src/vtb/baranovdv/Main.java
@@ -0,0 +1,35 @@
+package vtb.baranovdv;
+
+//Написать метод, который возвращает множество, в котором удалены все элементы четной длины из исходного множества.
+//public Set<String> removeEvenLength(Set<String> set);
+//Например, для множества ["foo", "buzz", "bar", "fork", "bort", "spoon", "!", "dude"] метод вернет ["foo", "bar", "spoon", "!"]
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void removeEvenLength(ArrayList<String> stringList) {
+        for (int i = 0; i < stringList.size(); i++) {
+            String word = stringList.get(i);
+            if (word.length() % 2 == 0) {//even
+                stringList.remove(word);//if it is
+                i = i - 1;
+                continue;
+            }
+        }
+
+        for (int i = 0; i < stringList.size(); i++) {
+            String word = stringList.get(i);
+            System.out.println(word);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> stringList = new ArrayList<String>();
+        stringList.add("носки");
+        stringList.add("футболка");
+        stringList.add("майка");
+        stringList.add("трусы");
+        stringList.add("перчатки");
+        removeEvenLength(stringList);
+    }
+}
